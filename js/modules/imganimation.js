cf991cd13faf2dbcbf927a5963d63c46d60baa6d
Jie Yang <112998909+Chris408-hub@users.noreply.github.com>
2024-03-19 19:58:34 -0400

    Update imganimation.js

--- js/modules/imganimation.js
+++ js/modules/imganimation.js
@@ -16,20 +16,6 @@ gsap.from(".self img", {
   duration: 1.5 
 });
 
-// tool icon animation
-document.querySelectorAll(".tool-icon").forEach((icon, index) => {
-  gsap.from(icon, {
-    scrollTrigger: {
-      trigger: icon,
-      toggleActions: "play none none none",
-      start: "top 90%", 
-    },
-    opacity: 0,
-    rotation: 180,
-    duration: 1 + index * 0.2, 
-  });
-});
-
 
 
 
